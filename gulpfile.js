157af6c93cb365a81392909a16ae8c6ba019a9e3
igo7 <mesoeuphoria@gmail.com>
2018-04-07 16:09:12 -0700

    part 1 + 2

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon');
+
+
+gulp.task('default', function(){
+nodemon({
+        script: 'app.js',
+        exe: 'js',
+        env: {
+            PORT:8000
+        },
+        ignore: ['./node_modules/**']
+    })
+    .on('restart', function(){
+        console.log('Gulp is running my app on PORT');
+    });
+});
